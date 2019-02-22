76f9805fcdfc11791999bb8cde616433fbf4f751
Alexander <alexander@boeckle.de>
2019-02-22 13:13:04 +0100

    Tag 1 Mittag

--- Auto/src/Auto.java
+++ Auto/src/Auto.java
@@ -4,11 +4,35 @@ public class Auto {
     String modell;
     String farbe;
     String serienNummer;
-    // Zwei weitere Attribut
-    // Tankvolumen und Verbrauch
+
+    double tankVolumen;     // Angabe in l
+    double verbrauch;       // Angabe in l / 100km
+    double tankStand;
+
+
 
     // Methoden
     // Berechnung der Reichweite
+    double gebeMaxReichweite() {
+        return tankVolumen / verbrauch * 100;
+    }
     // Tankvolumen / Verbrauch * 100
     // 30l / 6l/100km * 100 = 500km
+
+    void druckeInfo() {
+        System.out.printf("Der " + this.farbe + "e " + this.marke + " " + this.modell +  " kommt " + "%.0f" + "km weit.\n", this.gebeMaxReichweite());
+        System.out.println("Die Seriennummer lautet " + this.serienNummer);
+    }
+
+    double gebeAktReichweite() {
+        return tankStand / verbrauch * 100;
+    }
+
+    // fahre(Anzahl km)
+    // Soll berechen, wie viel Liter die gefahrene Strecke benötigt
+    // Und diesen Wert vom tankStand abziehen.
+
+    // tanke(Anzahl an litern)
+    // Die Liter sollen dem Tankstand hinzugefügt werden
+
 }
--- Auto/src/Main.java
+++ Auto/src/Main.java
@@ -17,20 +17,30 @@ public class Main {
         AudiA4.modell= "A4";
         AudiA4.farbe = "blau";
         AudiA4.serienNummer = "FZG7839038";
+        AudiA4.tankVolumen = 64;
+        AudiA4.verbrauch = 6.3;
 
         BMW3er.marke = "BMW";
         BMW3er.modell ="3er";
         BMW3er.farbe = "schwarz";
         BMW3er.serienNummer = "B3393788";
+        BMW3er.tankVolumen = 72;
+        BMW3er.verbrauch = 8.4;
 
         AudiA6.marke = "Audi";
         AudiA6.modell = "A6";
         AudiA6.farbe = "rot";
         AudiA6.serienNummer = "A4823D8";
+        AudiA6.verbrauch = 7.8;
+        AudiA6.tankVolumen = 82;
 
         System.out.println("In meiner Garage steht ...");
         System.out.println("... ein " + AudiA4.farbe + "er " + AudiA4.marke + " " + AudiA4.modell);
         System.out.println("... ein " + BMW3er.farbe + "er " + BMW3er.marke + " " + BMW3er.modell);
         System.out.println("... ein " + AudiA6.farbe + "er " + AudiA6.marke + " " + AudiA6.modell);
+
+        AudiA4.druckeInfo();
+        BMW3er.druckeInfo();
+        AudiA6.druckeInfo();
     }
 }
