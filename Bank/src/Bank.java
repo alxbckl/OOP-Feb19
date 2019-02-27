92182c386438b827642ed1276ff0198cd8690a04
Alexander <alexander@boeckle.de>
2019-02-27 10:45:17 +0100

    Update Bank.java

--- Bank/src/Bank.java
+++ Bank/src/Bank.java
@@ -1,4 +1,5 @@
-
+// Importieren des Scanners
+import java.util.Scanner;
 
 public class Bank {
     // Attribute
@@ -17,68 +18,130 @@ public class Bank {
     Account account1 = new Account();
     Account account2 = new Account();
 
+    // Scanner
+    Scanner sc = new Scanner(System.in);
+
+    // Zustandasvariable
+    int state = 0;
+
+    // Zustände definieren
+    final int WELCOME = 0;  // Bank eröffnen
+    final int MENU = 1;     // Auswahlmenü, was soll gemacht werden?
+    final int NEWCUST = 2;  // Neuen Kunden anlegen
+    final int WITHDRAW = 3; // Geld abheben
+    final int DEPOSIT = 4;  // Geld einzahlen
+    final int TRANSFER = 5; // Geld überweisen
+    final int AGAIN = 6;    // Weiter Aktionen?
+    final int CLOSE = 7;    // Bank schließen
+
 
     // Methoden
     public void start() {
+//        System.out.println("Comcave Bank");
+//        newLine();
+//
+//        // Kunde geht in die Bank und eröffnet neues Konto.
+//        // Kunde 0
+//        makeNewCustomer(customer0, "Max", "Mustermann", 0000);
+//        makeNewAccount(account0, 1000, 0000);
+//        // Kunde 1
+//        makeNewCustomer(customer1, "Peter", "Pan", 0001);
+//        makeNewAccount(account1, 1001, 0001);
+//        // Kunde 2
+//        makeNewCustomer(customer2, "Martha", "Maier", 0002);
+//        makeNewAccount(account2, 1002, 0002);
+//
+//        // Kunden zahlen ein
+//        // Kunde0 zahlt 3x50€ auf sein Konto ein
+//        depositMoney(account0, customer0, 50);
+//        depositMoney(account0, customer0, 50);
+//        depositMoney(account0, customer0, 50);
+//        newLine();
+//
+//        // Kunde1 zahlt 370€ auf sein Konto ein
+//        depositMoney(account1, customer1, 370);
+//        withdrawMoney(account1, customer1, 550);
+//
+//        newLine();
+//
+//        // Kunde 2 zahl 65€ auf sein Konto ein
+//        depositMoney(account2, customer2, 65000);
+//        withdrawMoney(account2, customer2, 12000);
+//
+//        newLine();
+//
+//        // Kundeninfo ausgeben
+//        printCustomerInfo(customer0);
+//        printAccountInfo(account0);
+//        newLine();
+//
+//        printCustomerInfo(customer1);
+//        printAccountInfo(account1);
+//        newLine();
+//
+//        printCustomerInfo(customer2);
+//        printAccountInfo(account2);
+//        newLine();
+//
+//        transferMoney(account0, account1, customer0, customer1, 5);
+//        newLine();
+//
+//        printCustomerInfo(customer0);
+//        printAccountInfo(account0);
+//        newLine();
+//
+//        printCustomerInfo(customer1);
+//        printAccountInfo(account1);
+//        newLine();
+//
+//        printCustomerInfo(customer2);
+//        printAccountInfo(account2);
+//        newLine();
+
+        // Zustandsautomat
+        // Mögliche Zustände:
+        // Eröffnen der Bank
+        // Erstellen eines neuen Kunden - Optional: Automatische Konten- und Kundennummernvergabe static als Hinweis
+        // Eingabe des Namens / der Kontonummer
+        // Auswahl der Optionen: Auszahlen, Einzahlen, Überweisen
+        // Nochmal?
+        // Programm beenden
+
+        while(state <= CLOSE) {
+            switch(state) {
+                case WELCOME:
+                    welcome();
+                    state = MENU;
+                    break;
+                case MENU:
+
+                    break;
+                case NEWCUST:
+
+                    break;
+                case WITHDRAW:
+
+                    break;
+                case DEPOSIT:
+
+                    break;
+                case TRANSFER:
+
+                    break;
+                case AGAIN:
+
+                    break;
+                case CLOSE:
+
+                    break;
+            }
+        }
+
+    }
+
+    private void welcome() {
         System.out.println("Comcave Bank");
-        newLine();
-
-        // Kunde geht in die Bank und eröffnet neues Konto.
-        // Kunde 0
-        makeNewCustomer(customer0, "Max", "Mustermann", 0000);
-        makeNewAccount(account0, 1000, 0000);
-        // Kunde 1
-        makeNewCustomer(customer1, "Peter", "Pan", 0001);
-        makeNewAccount(account1, 1001, 0001);
-        // Kunde 2
-        makeNewCustomer(customer2, "Martha", "Maier", 0002);
-        makeNewAccount(account2, 1002, 0002);
-
-        // Kunden zahlen ein
-        // Kunde0 zahlt 3x50€ auf sein Konto ein
-        depositMoney(account0, customer0, 50);
-        depositMoney(account0, customer0, 50);
-        depositMoney(account0, customer0, 50);
-        newLine();
-
-        // Kunde1 zahlt 370€ auf sein Konto ein
-        depositMoney(account1, customer1, 370);
-        withdrawMoney(account1, customer1, 550);
-
-        newLine();
-
-        // Kunde 2 zahl 65€ auf sein Konto ein
-        depositMoney(account2, customer2, 65000);
-        withdrawMoney(account2, customer2, 12000);
-
-        newLine();
-
-        // Kundeninfo ausgeben
-        printCustomerInfo(customer0);
-        printAccountInfo(account0);
-        newLine();
-
-        printCustomerInfo(customer1);
-        printAccountInfo(account1);
-        newLine();
-
-        printCustomerInfo(customer2);
-        printAccountInfo(account2);
-        newLine();
-
-        transferMoney(account0, account1, customer0, customer1, 5);
-        newLine();
-
-        printCustomerInfo(customer0);
-        printAccountInfo(account0);
-        newLine();
-
-        printCustomerInfo(customer1);
-        printAccountInfo(account1);
-        newLine();
-
-        printCustomerInfo(customer2);
-        printAccountInfo(account2);
+        System.out.println("Herzlich willkommen!");
         newLine();
     }
 
