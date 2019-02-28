4a008dbc548d6c4bc84e951b5220af2a20174124
Alexander <alexander@boeckle.de>
2019-02-28 13:05:03 +0100

    Tag 5 Vormittag

--- Geometry/src/Main.java
+++ Geometry/src/Main.java
@@ -1,33 +1,43 @@
 // Scanner einbinden
+import org.w3c.dom.css.Rect;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Ein Objekt vom Scanner erstellen
-        Scanner input = new Scanner(System.in);
         // Ein neues Objekt erzeugen
-        Rectangle rect1 = new Rectangle("DIN A4");
-        Rectangle rect2 = new Rectangle("Schwimmbad");
-        Rectangle rect3 = new Rectangle("Grundfläche");
-        Rectangle rect4 = new Rectangle("Garten");
+//        Rectangle rect1 = new Rectangle("DIN A4");
+//        Rectangle rect2 = new Rectangle("Schwimmbad");
+//        Rectangle rect3 = new Rectangle("Grundfläche");
+//        Rectangle rect4 = new Rectangle("Garten");
+//
+//        // Attribute setzen
+//        rect1.readAttributes();
+//        rect2.readAttributes();
+//        rect3.readAttributes();
+//        rect4.readAttributes();
+//
+//        // Rechteck zeichnen - Methoden ausführen
+//        rect1.printRectangle();
+//        rect1.printWidthLength();
+//
+//        rect2.printRectangle();
+//        rect2.printWidthLength();
+//
+//        rect3.printRectangle();
+//        rect3.printWidthLength();
+//
+//        rect1.printNonFilledRectangle();
 
-        // Attribute setzen
+        Triangle triang1 = new Triangle("Pyramide");
+        triang1.readAttributes();
+        triang1.printTriangleCentered();
+        triang1.printTriangleLeft();
+        triang1.printTriangleRight();
+
+        Rectangle rect1 = new Rectangle("Garten");
         rect1.readAttributes();
-        rect2.readAttributes();
-        rect3.readAttributes();
-        rect4.readAttributes();
-
-        // Rechteck zeichnen - Methoden ausführen
         rect1.printRectangle();
-        rect1.printWidthLength();
-
-        rect2.printRectangle();
-        rect2.printWidthLength();
-
-        rect3.printRectangle();
-        rect3.printWidthLength();
-
-        rect1.printNonFilledRectangle();
     }
 }
--- Geometry/src/Triangle.java
+++ Geometry/src/Triangle.java
@@ -1,3 +1,58 @@
-public class Triangle {
+// Scanner einbinden
+import java.util.Scanner;
 
+public class Triangle {
+    // Attribute
+    private int height;
+    private String name;
+    private Scanner sc = new Scanner(System.in);
+
+    // Methoden
+    public Triangle(String name) {
+        this.name = name;
+    }
+    public void readAttributes() {
+        System.out.println("Input for " + Colors.RED + name);
+        System.out.print(Colors.YELLOW + "Height: " + Colors.RESET);
+        // Height setzen
+        height = sc.nextInt();
+    }
+    public void printTriangleCentered() {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
+    public void printTriangleRight() {
+        for(int i = 1; i <= height; i++) {
+            // 1. Leerzeichen einfügen
+            for(int j = height - i; j >= 1; j -- ) {
+                System.out.print("  ");
+            }
+            // 2. Rauten einfügen
+            for (int j = 1; j <= i; j++) {
+                System.out.print("# ");
+            }
+
+
+
+            System.out.println();
+        }
+    }
+    public void printTriangleLeft() {
+        // Zeilen
+        for(int i = 1; i <= height; i++) {
+            // Zeichen innerhalb einer Zeile
+            for (int j = 1; j <= i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
 }
+
