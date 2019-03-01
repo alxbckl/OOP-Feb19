ef5a90cae3aa62077e7dcabb3de4aabee5fab443
Alexander <alexander@boeckle.de>
2019-03-01 10:51:57 +0100

    Update Main.java

--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -8,29 +8,81 @@ public class Main {
         // Größe ist veränderbar
         // Kann alle Datentypen und Klassenaufnehmen
 
-        // ArrayList<Datentyp> name = new ArrayList();
-        ArrayList<String> liste = new ArrayList();
 
         // Zum Vergleich ein herkömmliches Array
-        String[] list = new String[6];
+        // String[] list = new String[6];
 
-        // Neue Einträge ergänzen
-        liste.add("Hallo");
-        liste.add("Welt");
-        liste.add("!");
-        liste.add("Wie");
-        liste.remove(2);
-        liste.add(1, "Test");
+        // ArrayList<Datentyp> name = new ArrayList();
+        // Liste deklarieren
+        ArrayList<String> namen = new ArrayList();
+        namen.add("Ali Anani");
+        namen.add("Antonio Hägele");
+        namen.add("Damit Ayane");
+        namen.add("Ferhat Derin");
+        namen.add("Hamza Findik");
+        namen.add("Ingo Türke");
 
-        // Gibt die komplette liste aus
-        System.out.println(liste);
-
-        // Gibt die komplette liste auf unserem bisherigen Weg aus
-        for(int i = 0; i < liste.size(); i++) {
-            System.out.println(liste.get(i));
+        // Liste ausgeben
+        for(int i = 0; i < namen.size(); i++) {
+            System.out.println(namen.get(i));
         }
 
+        // Leere Zeile
+        System.out.println();
 
+        // Objekte Entferen
+        namen.remove(1);
+        namen.remove(3);
 
+        // Liste ausgeben
+        for(int i = 0; i < namen.size(); i++) {
+            System.out.println(namen.get(i));
+        }
+
+        // Leere Zeile
+        System.out.println();
+
+        namen.add(1, "Antonio Hägele");
+        namen.add(3, "Hamza Findik");
+
+        // Liste ausgeben
+        for(int i = 0; i < namen.size(); i++) {
+            System.out.println(namen.get(i));
+        }
+
+        // Leere Zeile
+        System.out.println();
+
+        // Liste durchsuchen
+
+        // Jedes mal mit if prüfen, ob der Wert gefunden wurde, der gesucht war
+        // Index auf der Konsole ausgeben
+        // Mit einer For schleife durch die Liste gehen
+        for(int i = 0; i < namen.size(); i++) {
+            if(namen.get(i).equals("Ferhat Derin")) {
+                System.out.println("Index: " + i);
+                break;
+            }
+        }
+
+        // Leere Zeile
+        System.out.println();
+
+        // Mit ausgelagerter Methode durchsuchen
+        System.out.println(searchFor("Ferhat Derin", namen));
+        System.out.println(searchFor("fjjl", namen));
+    }
+
+    // Nimmt einen Suchbegriff und eine ArrayListe in der gesucht werden soll an.
+    // Gibt den Index zurück, wenn der Suchbegriff gefunden wurde
+    // und -1, wenn er nicht gefunden wurde.
+    static int searchFor(String search, ArrayList namen) {
+        for(int i = 0; i < namen.size(); i++) {
+            if(namen.get(i).equals(search)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
